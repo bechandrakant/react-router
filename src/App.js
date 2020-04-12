cc1744ac4646b49c2fa11e830786dc5882b97eb6
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-04-12 10:36:12 +0530

    basic routing done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/about' component={About}></Route>
+      <Route exact path='/contact' component={Contact}></Route>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
