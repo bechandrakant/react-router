d7ac5e76acf3882b2eb565c5121e6cdabe0633da
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-04-12 23:23:49 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import Post from './components/Post'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Route exact path='/' component={Home}></Route>
-      <Route exact path='/about' component={About}></Route>
-      <Route exact path='/contact' component={Contact}></Route>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/about' component={About}></Route>
+        <Route exact path='/contact' component={Contact}></Route>
+        <Route path='/:post_id' component={Post}></Route>
+      </Switch>
     </BrowserRouter>
   )
 }
